25dbf37f36279b51a9163e0671d42927509c367b
Ben Allen <ben05allen@hotmail.com>
2025-12-03 16:29:11 +0900

    adding colour to challenge tests

--- tests.py
+++ tests.py
@@ -1,4 +1,14 @@
+from functools import partial
 from score_card import full_score
+
+
+def in_colour(text: str, colour: int) -> str:
+    return f"\033[{colour}m{text}\033[0m"
+
+
+in_green = partial(in_colour, colour=32)
+in_red = partial(in_colour, colour=31)
+
 
 TEST_CARDS = {
     # perfect game
@@ -29,15 +39,22 @@ TEST_CARDS = {
     "X 7/ 9- X -8 8/ - 72 X X81": 140,
 }
 
-for test_num, (card, pins) in enumerate(TEST_CARDS.items()):
+
+passed_tests = 0
+for test_num, (card, pins) in enumerate(TEST_CARDS.items(), start=passed_tests + 1):
     try:
-        assert full_score(card) == pins
-        print(f"Success: {card}")
-    except AssertionError as ae:
-        print(f"Failed on test {test_num} with card {card}\n{ae}")
-        raise
+        returned = full_score(card)
+        assert returned == pins
+        print(f"{test_num:>2} - {in_green('Success')} with card {card}")
+        passed_tests += 1
+    except AssertionError:
+        print(
+            f"{test_num:>2} - {in_red('Failed')} on test {test_num} with card {card}\n\tExpected {in_green(pins)} but got {in_red(returned)}"
+        )
+
     except Exception:
         print("Unexpected Error")
         raise
 
-    print(f"All {test_num} tests passed.")
+if passed_tests == test_num:
+    print(f"\n{in_green(f'{test_num} tests passed')}.")
